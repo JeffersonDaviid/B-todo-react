0054b9eddd508a115e3be3886528152764d69939
Jefferson David <jeffersonchile2001@gmail.com>
2022-10-23 20:00:56 -0500

    login-incomplete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
 import ListaTareas from './components/ListaTareas';
+import LogIn from './components/LogIn';
 
 function App() {
   return (
     <div className='App'>
+      <LogIn />
       <h1 className='title'>
         Todo App <span className='Logo-X'>X</span> Jefferson
       </h1>
---
+++ src/components/LogIn.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import '../stylesheets/logIn.css';
+import {
+  AiFillApple,
+  AiFillCheckCircle,
+  AiFillCheckSquare,
+  AiOutlineLock,
+  AiOutlineMail,
+  AiOutlineClose,
+} from 'react-icons/ai';
+import { GrGoogle } from 'react-icons/gr';
+import { FaFacebookF } from 'react-icons/fa';
+
+const LogIn = () => {
+  return (
+    <div className='login'>
+      <form className='login-user'>
+        <AiOutlineClose className='login-user__close' />
+        <div className='login-user__logo'>
+          <img
+            src='https://global-uploads.webflow.com/5f84bfe6967b7d35c51b76aa/5f84ce91b8caf04e6bf05e09_Jones-White-Logo.png'
+            alt='logo empresa'
+            className='login-user__img'
+          ></img>
+          <p className='login-user__text' placeholder='example@gmail.com'>
+            Jefferson David
+          </p>
+        </div>
+        <p className='login-user__label'>Username</p>
+        <div className='user-email'>
+          <AiOutlineMail className='user-email__icon' />
+          <input type='email' className='user-email__input' required />
+          <AiFillCheckCircle className='user-email__check' />
+        </div>
+        <p className='login-password__label'>Password</p>
+        <div className='user-password'>
+          <AiOutlineLock className='user-password__icon' />
+          <input
+            type='password'
+            className='user-password__input'
+            required
+            placeholder='xxxxxxx'
+          />
+        </div>
+        <div className='login-actions'>
+          <div className='remember-user'>
+            <AiFillCheckSquare className='remember-user__icon' />
+            <p className='remember-user__text'>Remember me</p>
+          </div>
+          <p className='forgot-password'>Forgot password</p>
+        </div>
+        <button className='btn-submit'>Sing in</button>
+      </form>
+      <div className='way-signUp'>
+        <GrGoogle className='way-signUp__google way-signUp__icon' />
+        <FaFacebookF className='way-signUp__facebook way-signUp__icon' />
+        <AiFillApple className='way-signUp__apple way-signUp__icon' />
+      </div>
+      <p className='way-signUp__text'>
+        Don't have an account?{' '}
+        <span className='way-signUp__text--yellow'> Sing Up here</span>
+      </p>
+    </div>
+  );
+};
+
+export default LogIn;
